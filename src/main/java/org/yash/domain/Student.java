4a556744c48ebe20f929ca5ed1ca5a876fa7dd68
Gaurav Kamra <kamra.gaurav515@gmail.com>
2021-06-06 12:05:38 +0530

    autowired byName

--- src/main/java/org/yash/domain/Student.java
+++ src/main/java/org/yash/domain/Student.java
@@ -7,6 +7,33 @@ public class Student {
     private String studentName;
     private Address address;
 
+    public Student() {
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
     public Student(int studentId, String studentName, Address address) {
         this.studentId = studentId;
         this.studentName = studentName;
